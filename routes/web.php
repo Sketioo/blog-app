61966d6e1d4f9b6f6b26b12aafc044c28cfed82b
AmurTigro <martiohusein27@gmail.com>
2024-06-22 19:34:04 +0700

    "Modified API and web routes: commented out user route in api.php, updated welcome route and added new routes in web.php"

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,18 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home.index');
+})->name('home.index');
+
+//* contact, posts/{id}
+
+Route::get('/contact', function () {
+    return view('home.contact');
+})->name('home.contact');
+
+Route::get('/posts/{id}', function ($id) {
+    return "this is post with id: $id";
+})->name('posts.show');
