d1a3662d59bf7e19e5f0235a144c3c3f4c96c84a
AmurTigro <martiohusein27@gmail.com>
2024-06-22 19:37:22 +0700

    "Refactored contact and index blade templates to extend layouts.app and use section directives."

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Contact</title>
-</head>
-<body>
-  <h1>This is Contact Page</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+
+@section('title', 'Contact')
+
+@section('content')
+  <h1>Contact page</h1>
+@endsection
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Homepage</title>
-</head>
-<body>
-  <h1>This is Homepage</h1>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'Homepage')
+
+@section('content')
+  <h1>Homepage</h1>
+@endsection
