fa91f7ec8bd627ae5c1307cd6c23c4f222f3c677
AmurTigro <martiohusein27@gmail.com>
2024-06-29 23:53:00 +0700

    "Updated CSS, JS, and Blade templates for homepage and contact page"

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -1,10 +1,48 @@
 @extends('layouts.app')
 
-
 @section('title', 'Contact')
 
 @section('content')
-  <div class="container my-5">
-    <h1>Contact</h1>
-  </div>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Contact Us</h2>
+                <p>Feel free to reach out to us using the information below or by filling out the contact form.</p>
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <i class="fas fa-phone-alt mr-2"></i> +1 234-567-8900
+                    </li>
+                    <li class="list-group-item">
+                        <i class="fas fa-envelope mr-2"></i> info@yourcompany.com
+                    </li>
+                    <li class="list-group-item">
+                        <i class="fas fa-map-marker-alt mr-2"></i> 123 Main Street, Anytown, CA 12345
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-md-6">
+                <h2>Get in Touch</h2>
+                <form action="" method="post">
+                    @csrf
+                    <div class="form-group mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="name" name="name" placeholder="Your Name"
+                            required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="email" class="form-label">Email Address</label>
+                        <input type="email" class="form-control" id="email" name="email"
+                            placeholder="youremail@example.com" required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label for="message" class="form-label">Message</label>
+                        <textarea class="form-control" id="message" name="message" rows="5" placeholder="Write your message here"
+                            required></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Send Message</button>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,9 +1,58 @@
 @extends('layouts.app')
 
-@section('title', 'Homepage')
+@section('title', 'Welcome to [Website Name]')
 
 @section('content')
     <div class="container my-5">
-        <h1>Homepage</h1>
+        <div class="row align-items-center">
+            <div class="col-md-8">
+                <h1 class="display-4">Explore the World of Programming</h1>
+                <p class="lead">Stay up-to-date with the latest trends and insights in the ever-evolving world of
+                    programming. We publish informative and engaging blog posts on various programming topics to help you
+                    learn, grow, and excel as a developer.</p>
+                <a href="{{ route('posts.index') }}" class="btn btn-primary btn-lg">Browse Recent Posts</a>
+            </div>
+
+            <div class="col-md-4 d-none d-md-block">
+                <img src="{{ asset('https://picsum.photos/id/1026/600/400') }}" alt="Programming Illustration"
+                    class="img-fluid rounded-end">
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-md-4">
+                <div class="card shadow-sm">
+                    <img src="{{ asset('https://picsum.photos/id/1027/300/200') }}" alt="Blog Post 1" class="card-img-top">
+                    <div class="card-body">
+                        <h3><a href="#">Intriguing Headline for Programming Blog Post 1</a></h3>
+                        <p class="card-text">A short and captivating excerpt to entice readers to learn more about Blog Post
+                            1.</p>
+                        <a href="#" class="btn btn-primary">Read More</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card shadow-sm">
+                    <img src="{{ asset('https://picsum.photos/id/1028/300/200') }}" alt="Blog Post 2" class="card-img-top">
+                    <div class="card-body">
+                        <h3><a href="#">Intriguing Headline for Programming Blog Post 2</a></h3>
+                        <p class="card-text">A short and captivating excerpt to entice readers to learn more about Blog Post
+                            2.</p>
+                        <a href="#" class="btn btn-primary">Read More</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card shadow-sm">
+                    <img src="{{ asset('https://picsum.photos/id/1029/300/200') }}" alt="Blog Post 3" class="card-img-top">
+                    <div class="card-body">
+                        <h3><a href="#">Intriguing Headline for Programming Blog Post 3</a></h3>
+                        <a href="#" class="btn btn-primary">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
